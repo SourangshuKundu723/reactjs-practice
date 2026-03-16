01318a42c2aa1bc2136aad9dcca27b5321236739
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-03-16 11:41:14 +0530

    Adding variable in App.jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
 import Greet from './Greet.jsx'
 
 function App() {
+  const username = "Sourangshu"
+
   return (
     <>
       <h1>Hello Werld!</h1>
       <Greet />
+      <h3>Hi! I am {username}</h3>
     </>
   )
 }
