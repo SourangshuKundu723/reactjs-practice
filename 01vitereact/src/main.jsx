1ff9758d6b1b04bfe81ed8b9739b5d689d249ef3
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-03-16 11:34:34 +0530

    Creating custom React Element

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//Custom React Element
+const reactElement = React.createElement(
+  'a', 
+  {href: 'https://www.google.com', target: '_blank'},
+  'visit Google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
